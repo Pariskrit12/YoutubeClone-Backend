ee017d70f3cff514f9ad78cb17a7c81b4b5dfb18
Pariskrit12 <bhagataryan784@gmail.com>
2025-05-26 16:21:21 +0545

    Set up Express app with middleware: CORS, JSON parsing, URL-encoded data, static files, and cookie parser

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser"
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}));// Parse incoming JSON requests and limit the payload size to 16KB for security and performance
+app.use(express.urlencoded({extended:true,limit:"16kb"}));// Parse URL-encoded data (from forms) with extended support for rich objects, limit payload to 16KB
+app.use(express.static("public"));// Serve static files (HTML, CSS, JS, images, etc.) from the "public" directory
+app.use(cookieParser());// Parse cookies from the HTTP request headers and make them accessible via req.cookies
+
+export {app};
