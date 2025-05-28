f9d08e61cfa475c1276ed7a740361164bbebde1a
Pariskrit12 <bhagataryan784@gmail.com>
2025-05-28 23:32:04 +0545

    Added route for login and protected route for logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { register } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, register } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router=Router();
 
-router.route("/register").post(upload.single("avatar")
-,register)
+router.route("/register").post(upload.single("avatar"),register);
+router.route("/login").post(loginUser);
+
+
+//Protected Routes
+router.route("/logout").post(verifyJwt,logoutUser);
+
 export default router;
