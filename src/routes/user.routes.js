410fe56fd129cfd4745f5c14a115db38d0c9b5c6
Pariskrit12 <bhagataryan784@gmail.com>
2025-05-29 23:16:58 +0545

    add routes to fetch personal and public user info

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, register } from "../controllers/user.controller.js";
+import { getPersonalInfo, loginUser, logoutUser, register,getUserInfo } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router=Router();
 
 router.route("/register").post(upload.single("avatar"),register);
 router.route("/login").post(loginUser);
+router.route("/get-user-info/:userId").get(getUserInfo)
+
 
 
 //Protected Routes
 router.route("/logout").post(verifyJwt,logoutUser);
+router.route("/get-personal-info").get(verifyJwt,getPersonalInfo);
 
 export default router;
