d039dc2d1ca33a2b0beeb958fab02ef8ef5ab726
Pariskrit12 <bhagataryan784@gmail.com>
2025-05-30 20:23:23 +0545

    Written the routes for subscribing,unsubscribing and get details about the users subscribed channel

--- src/routes/channel.routes.js
+++ src/routes/channel.routes.js
@@ -3,10 +3,13 @@ import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { createChannel, deleteChannel, getChannelInfo, updateAvatarOfChannel, updateBannerofChannel, updateChannel } from "../controllers/channel.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
+import { getSubscribedChannelOfLoggedInUser, subscribeToChannel, unsubscribeToChannel } from "../controllers/subscription.controller.js";
+
 
 const router=Router();
 
-router.route("/get-channel-info/:channelId").get(getChannelInfo);
+
+router.route("/get-channel-info/:channelId").get(verifyJwt,getChannelInfo);
 
 //protected routes
 router.route("/create-channel").post(verifyJwt,upload.fields([
@@ -23,5 +26,8 @@ router.route("/create-channel").post(verifyJwt,upload.fields([
 router.route("/delete-channel/:channelId").post(verifyJwt,deleteChannel);
 router.route("/update-channel/:channelId").put(verifyJwt,updateChannel);
 router.route("/update-channel-avatar/:channelId").put(verifyJwt,upload.single("avatar"),updateAvatarOfChannel);
-router.route("/update-channel-banner/:channelId").put(verifyJwt,upload.single("banner"),updateBannerofChannel)
+router.route("/update-channel-banner/:channelId").put(verifyJwt,upload.single("banner"),updateBannerofChannel);
+router.route('/subscribe-channel/:channelId').put(verifyJwt,subscribeToChannel)
+router.route('/unsubscribe-channel/:channelId').put(verifyJwt,unsubscribeToChannel);
+router.route("/subscribed-channel").get(verifyJwt,getSubscribedChannelOfLoggedInUser);
 export default router;
