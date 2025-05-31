9a69fa7b0d0e55203d723b439a0f06d31e187614
Pariskrit12 <bhagataryan784@gmail.com>
2025-05-31 20:07:38 +0545

    removed and pushed config by removing .git

---
+++ src/config/redis.js
@@ -0,0 +1,17 @@
+import Redis from "ioredis";
+
+// Create a Redis client instance and connect to local Redis server at 127.0.0.1:6379
+const redisClient = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+//error handling
+redisClient.on("error", (err) => {
+  console.error("Redis Error:", err);
+});
+
+redisClient.on("connect",()=>{
+    console.error("Connected to redis");
+})
+export {redisClient};
