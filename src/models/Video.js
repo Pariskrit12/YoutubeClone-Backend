c728f34205c1399bdcc6c9f9c81ecd8900247e3f
Pariskrit12 <bhagataryan784@gmail.com>
2025-05-31 15:18:26 +0545

    Added duration in the videoSchema and added index for fast search

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,66 +1,77 @@
 import mongoose, { Schema } from "mongoose";
 
-const videoSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+const videoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-  description: {
-    type: String,
-    trim: true,
-  },
-
-  videoUrl: {
-    type: String,
-    required: true,
-  },
-
-  thumbnail: {
-    type: String,
-    default: "",
-  },
-
-  channel: {
-    type: Schema.Types.ObjectId,
-    ref: "Channel",
-    required: true,
-  },
-
-  views: {
-    type: Number,
-    default: 0,
-  },
-
-  likes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    }
-  ],
-
-  dislikes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    }
-  ],
-
-  comments: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Comment",
-    }
-  ],
-
-  tags: [
-    {
+    description: {
       type: String,
       trim: true,
-    }
-  ]
+    },
 
-}, { timestamps: true });
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    videoPublicId: {
+      type: String,
+    },
+
+    thumbnail: {
+      type: String,
+      default: "",
+    },
+
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "Channel",
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    dislikes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    duration:{
+      type:Number,
+      default:0
+    }
+  },
+  { timestamps: true }
+);
+videoSchema.index({title:"text",description:"text"});
+videoSchema.index({tags:1});
 
 export const Video = mongoose.model("Video", videoSchema);
