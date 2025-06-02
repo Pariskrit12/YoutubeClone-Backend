a33696ab8098d97d0bbe63b7f59daa22da466255
Pariskrit12 <bhagataryan784@gmail.com>
2025-06-02 17:35:29 +0545

    Written routes for updating video information

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
-import { uploadVideo } from "../controllers/video.controller.js";
+import { getVideoInfo, updateVideoInfo, updateVideoThumbnail, uploadVideo } from "../controllers/video.controller.js";
 const router = Router();
 
 
 //protected routes
+router.route("/update-video-thumbnail/:videoId").put(verifyJwt,upload.single("thumbnail"),updateVideoThumbnail);
+router.route("/update-video-info/:videoId").put(verifyJwt,updateVideoInfo);
+router.route('/get-info-video/:videoId').get(verifyJwt,getVideoInfo);
 router.route("/upload-video/:channelId").post(
   verifyJwt,
   upload.fields([
