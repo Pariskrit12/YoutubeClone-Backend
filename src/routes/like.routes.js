f3ac61089fde07c2f6c18c0958630bc2b3fb5429
Pariskrit12 <bhagataryan784@gmail.com>
2025-06-04 16:27:09 +0545

    Written routes for liking and disliking comments

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { dislikeVideo, likeVideo } from "../controllers/like.controller.js";
+import { dislikeComment, dislikeVideo, likeComment, likeVideo } from "../controllers/like.controller.js";
 
 const router=Router();
 
 router.route('/like-video/:videoId').post(verifyJwt,likeVideo);
 router.route('/dislike-video/:videoId').post(verifyJwt,dislikeVideo);
+router.route('/dislike-comment/:commentId').post(verifyJwt,dislikeComment);
+router.route('/like-comment/:commentId').post(verifyJwt,likeComment);
 
 export default router;
