a7bf8c4ce406040948b0989bc431741db3db1270
Pariskrit12 <bhagataryan784@gmail.com>
2025-06-07 00:00:55 +0545

    Add moderation fields to Comment schema

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,61 +1,71 @@
 import mongoose, { Schema } from "mongoose";
 
-const commentSchema = new Schema({
-  content: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-  authorId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-
-  videoId: {
-    type: Schema.Types.ObjectId,
-    ref: "Video",
-    required: true,
-  },
-
-  replies: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Comment",
-      default:null
-    }
-  ],
-
-  likes: [
-    {
+    authorId: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      default:[],
-    }
-  ],
-  dislikes: [
-    {
+      required: true,
+    },
+
+    videoId: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+
+    replies: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+        default: null,
+      },
+    ],
+
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    dislikes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+
+    isReported: {
+      type: Boolean,
+      default: false,
+    },
+
+    isApproved: {
+      type: Boolean,
+      default: true,
+    },
+    reportedReason: {
+      type: String,
+      default: "",
+    },
+    moderatedBy: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      default:[],
-    }
-  ],
-
-  likesComment:[
-    {
-      type:Schema.Types.ObjectId,
-      ref:"User",
-      default:[]
-    }
-  ],
-  dislikesComment:[
-    {
-      type:Schema.Types.ObjectId,
-      ref:"User",
-      default:[]
-    }
-  ]
-}, { timestamps: true });
+    },
+    moderationNotes: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Comment = mongoose.model("Comment", commentSchema);
