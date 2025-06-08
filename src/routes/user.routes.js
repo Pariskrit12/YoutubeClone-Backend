cdf7709e1ff808826199fc33b92981957a5f4559
Pariskrit12 <bhagataryan784@gmail.com>
2025-06-08 16:14:00 +0545

    Tested all the routes and added change password routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
-import { getPersonalInfo, loginUser, logoutUser, register,getUserInfo, updateUserInfo, updateUserAvatar } from "../controllers/user.controller.js";
+import { getPersonalInfo, loginUser, logoutUser, register,getUserInfo, updateUserInfo, updateUserAvatar, changePassword } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router=Router();
 
-router.route("/register").post(upload.single("avatar"),register);
-router.route("/login").post(loginUser);
-router.route("/get-user-info/:userId").get(getUserInfo)
+router.route("/register").post(upload.single("avatar"),register)//tested;
+router.route("/login").post(loginUser)//tested;
+router.route("/get-user-info/:userId").get(getUserInfo)//tested
 
 
 
 //Protected Routes
-router.route("/logout").post(verifyJwt,logoutUser);
-router.route("/get-personal-info").get(verifyJwt,getPersonalInfo);
-router.route("/update-personal-info").put(verifyJwt,updateUserInfo);
-router.route("/update-user-avatar").put(verifyJwt,upload.single("avatar"),updateUserAvatar);
-
+router.route("/logout").post(verifyJwt,logoutUser)//tested;
+router.route("/get-personal-info").get(verifyJwt,getPersonalInfo)//tested;
+router.route("/update-personal-info").put(verifyJwt,updateUserInfo)//tested;
+router.route("/update-user-avatar").put(verifyJwt,upload.single("avatar"),updateUserAvatar)//tested;
+router.route("/change-password").post(verifyJwt,changePassword);//tested
 export default router;
